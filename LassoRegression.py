cf3fc06ee724284ead17e8ca238897dfd9a25433
OIZHENHAU <zhenhau8072@gmail.com>
2024-05-03 15:56:37 +0800

    LassoRegression

---
+++ LassoRegression.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import re
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.linear_model import LassoCV
+from sklearn.model_selection import RepeatedKFold
+from numpy import arange
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Lasso
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
+
+
+def fit(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+
+class LassoRegressionCVModel:
+    def __init__(self, start_value, end_value, step_size, n_splits, n_repeats):
+        self.start_value = start_value
+        self.end_value = end_value
+        self.step_size = step_size
+        self.n_splits = n_splits
+        self.n_repeats = n_repeats
+        self.cv = RepeatedKFold(n_splits=n_splits, n_repeats=n_repeats)
+        self.model = LassoCV(alphas=arange(start_value, end_value, step_size), cv=self.cv)
+
+    def fit(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        y_pred = self.model.predict(X_test)
+        return y_pred
